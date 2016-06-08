403cca37799f7f4776408b2897c086d23b0d426f
tsuilouis <louis.tsui@mail.utoronto.ca>
2016-06-07 23:03:25 -0400

    Week 5 initial commit

---
+++ app/src/main/java/com/tsui/louis/modernartui/ModernArtActivity.java
@@ -0,0 +1,13 @@
+package com.tsui.louis.modernartui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModernArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modern_art);
+    }
+}
